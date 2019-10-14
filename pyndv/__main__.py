37ea882b34235322397db44dfe95f6c76d361ac2
vit0r <vitornascimentoaraujo@gmail.com>
2019-10-14 02:37:05 -0300

    include output to file

--- pyndv/__main__.py
+++ pyndv/__main__.py
@@ -9,10 +9,12 @@ from pyndv import core
 
 @click.command(name=__name__)
 @click.option('--feed', '-f', default='recent', help='Feed name', prompt='Selected feed: ')
-def main(feed=None):
+@click.option('--output', '-o', default='pyndv_output.json', help='output file name', prompt='Output file name: ',
+              required=True)
+def main(feed=None, output=None):
     """ Main entry point of the app """
     feed_processor = core.FeedProcessor()
-    feed_processor(feed_type=feed)
+    feed_processor(feed_type=feed, output=output)
     return feed_processor.feed_json
 
 
--- pyndv/core/__init__.py
+++ pyndv/core/__init__.py
@@ -4,63 +4,76 @@ import gzip
 import json
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
+from pathlib import Path
+
 import requests
 
 
 class FeedProcessor:
     """Process feed"""
 
-    __FEED_JSON_URL = 'https://nvd.nist.gov/feeds/json/cve/1.0/nvdcve-1.0-{}.json.gz'
+    _FEED_JSON_URL = 'https://nvd.nist.gov/feeds/json/cve/1.0/nvdcve-1.0-{}.json.gz'
 
-    def __init__(self):
+    def _init_(self):
         """Init self"""
-        self.__feed_json = None
-        self.__feed_resource_url = None
-        self.__feed_type = None
-        self.year = datetime.now().year
+        self._feed_json = None
+        self._feed_resource_url = None
+        self._feed_type = None
+        self._output_file = None
 
     def __repr__(self):
         """Representation of class"""
         return '{}()'.format(self.__class__.__name__)
 
+    def __call__(self, *args, **kwargs):
+        """get feed json by type"""
+        self._setdefault_feed_type(kwargs)
+        self._output_file = kwargs.get('output')
+        self._download_feed_resource()
+
     @property
     def feed_json(self):
         """Feed json data"""
-        return self.__feed_json
+        return self._feed_json
 
     @property
     def feed_resource_url(self):
         """Feed type url"""
-        return self.__feed_resource_url
+        return self._feed_resource_url
 
-    def __process_feed(self, process):
+    def _process_feed(self, process):
         response = process.result()
         if response.status_code != 200:
             raise ValueError('Error to request feed url')
         nvdcve_json_str = gzip.decompress(response.content)
-        self.__feed_json = json.loads(nvdcve_json_str)
+        self._feed_json = json.loads(nvdcve_json_str)
+        self._write_output_file()
 
-    def __download_feed_resource(self):
+    def _download_feed_resource(self):
         """Download feed resource by feed type"""
-        self.__feed_resource_url = self.__FEED_JSON_URL.format(
-            self.__feed_type)
+        self._feed_resource_url = self._FEED_JSON_URL.format(
+            self._feed_type)
         with ThreadPoolExecutor(max_workers=1) as processor:
             process = processor.submit(
-                requests.get, self.__feed_resource_url, stream=True)
-            print('Start download {}'.format(self.__feed_type))
+                requests.get, self._feed_resource_url, stream=True)
+            print('Start download {}'.format(self._feed_type))
             while process.running():
                 print('.', end='')
             if process.done():
                 print('Download complete.')
-            process.add_done_callback(self.__process_feed)
+            process.add_done_callback(self._process_feed)
 
-    def __call__(self, *args, **kwargs):
-        """get feed json by type"""
-        self.__setdefault_feed_type(kwargs)
-        self.__download_feed_resource()
-
-    def __setdefault_feed_type(self, kwargs):
+    def _setdefault_feed_type(self, kwargs):
         """Set default value if feed type not exists"""
-        self.__feed_type = kwargs.get('feed_type', None)
-        if self.__feed_type not in ['recent', 'modified', self.year]:
-            self.__feed_type = self.year
+        self._feed_type = kwargs.get('feed_type', None)
+        current_year = datetime.now().year
+        if self._feed_type not in ['recent', 'modified', current_year]:
+            self._feed_type = current_year
+
+    def _write_output_file(self):
+        assert self._output_file, 'output file needed'
+        output_file_path = Path(self._output_file)
+        if output_file_path.exists():
+            output_file_path = Path(output_file_path.parent, f"{datetime.utcnow().time()}.json")
+        data_str = json.dumps(self._feed_json, sort_keys=False, indent=2)
+        output_file_path.write_text(data_str)
